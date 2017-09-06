5faf158fdc4ce9819811594d9a22702fb73ae121
dannymasoft <danny@masoft.net>
2017-09-06 13:43:59 -0500

    roles

--- view/RolesView.php
+++ view/RolesView.php
@@ -1,161 +1,74 @@
-  <?php include("view/modulos/head.php"); ?>
-
 <!DOCTYPE HTML>
 <html lang="es">
-     <head>
-        <meta charset="utf-8"/>
-        <title>Roles - aDocument 2015</title>
-    <link rel="stylesheet" href="view/css/bootstrap.css">
-          <link rel="stylesheet" type="text/css" href="css/jquery-ui-1.7.2.custom.css" />
-          <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-          <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>  
-          <script src="view/css/jquery.js"></script>
-		  <script src="view/css/bootstrapValidator.min.js"></script>
-		  <script src="view/css/ValidarRoles.js"></script>
-      
-        <style>
-            input{
-                margin-top:5px;
-                margin-bottom:5px;
-            }
-            .right{
-                float:right;
-            }
-                
-            
-        </style>
+    <head>
+     <title>Bienvenido - aDocument 2015</title>
+      <?php include("view/modulos/links.php"); ?>
     </head>
-      <body style="background-color: #F6FADE">
-    
-     
-       
-       <?php include("view/modulos/menu.php"); ?>
-  
-    <div class="container">
-      <div class="row" style="background-color: #FAFAFA;">
-      
-      <form id="form-Roles" action="<?php echo $helper->url("Roles","InsertaRoles"); ?>" method="post" class="col-lg-5">
-            
-            	
-		   		
-            
-             <?php if ($resultEdit !="" ) { foreach($resultEdit as $resEdit) {?>
-	        
-	         <div class="panel panel-info">
-	         <div class="panel-heading">
-	         <h4><i class='glyphicon glyphicon-edit'></i> Editar Roles</h4>
-	         </div>
-	         <div class="panel-body">
-	        
-	        <div class="row">
-		    <div class="col-xs-12 col-md-12">
-		    <div class="form-group">
-                                  <label for="nombre_rol" class="control-label">Nombre Rol:</label>
-                                  <input type="text" class="form-control" id="nombre_rol" name="nombre_rol" value="<?php echo $resEdit->nombre_rol; ?>"  placeholder="Nombre Rol">
-                                  <span class="help-block"></span>
-            </div>
-		    </div>
-		     </div>
-	        
-	               
-		            
-		     <div class="row">
-		    <div class="col-xs-12 col-md-12 col-lg-12" style="text-align: center; margin-top:40px">
-		    <div class="form-group">
-                                 <button type="submit" id="Guardar" name="Guardar" class="btn btn-success">Guardar</button>
-            </div>
-		    </div>
-		    </div>
-		            
-		            
-		      </div>
-		      </div>      
-            
-		     <?php } } else {?>
-		     
-		       <div class="panel panel-info">
-	         <div class="panel-heading">
-	         <h4><i class='glyphicon glyphicon-edit'></i> Insertar Roles</h4>
-	         </div>
-	         <div class="panel-body">
-		    
-		     <div class="row">
-		    <div class="col-xs-12 col-md-12">
-		    <div class="form-group">
-                                  <label for="nombre_rol" class="control-label">Nombre Rol:</label>
-                                  <input type="text" class="form-control" id="nombre_rol" name="nombre_rol" value=""  placeholder="Nombre Rol">
-                                  <span class="help-block"></span>
-            </div>
-		    </div>
-		     </div>
-		       
-		    <div class="row">
-		    <div class="col-xs-12 col-md-12 col-lg-12" style="text-align: center; margin-top:40px">
-		    <div class="form-group">
-                                 <button type="submit" id="Guardar" name="Guardar" class="btn btn-success">Guardar</button>
-            </div>
-		    </div>
-		    </div>
-		       
-		       
-		    
-		       
-		       </div>
-		      </div>       
-		     <?php } ?>
-		        
-          
-          </form>
-       
-       
-        <div class="col-lg-7">
-         <div class="panel panel-info">
-	         <div class="panel-heading">
-	         <h4><i class='glyphicon glyphicon-edit'></i> Roles de Usuario</h4>
-	         </div>
-	         </div>
-		 </div>
-           <div class="panel-body">
-        <section class="col-lg-7" style="height:400px;overflow-y:scroll;">
-        <table class="table table-hover">
-	         <tr class="info">
-	    		<th style="text-align: left;  font-size: 13px;">Id</th>
-	    		<th style="text-align: left;  font-size: 13px;">Nombre Rol</th>
-	    		<th style="text-align: left;  font-size: 13px;"></th>
-	    		<th style="text-align: left;  font-size: 13px;"></th>
-	    	
-	  		</tr>
-            
-	            <?php foreach($resultSet as $res) {?>
-	        		<tr>
-	                   <td style="font-size: 11px;"> <?php echo $res->id_rol; ?>  </td>
-		               <td style="font-size: 11px;"> <?php echo $res->nombre_rol; ?>     </td> 
-		               
-		               <td style="font-size: 11px;">
-			           		<div class="right">
-			                    <a href="<?php echo $helper->url("Roles","index"); ?>&id_rol=<?php echo $res->id_rol; ?>" class="btn btn-warning" style="font-size:65%;"><i class='glyphicon glyphicon-edit'></i></a>
-			                </div>
-			            
-			             </td>
-			             <td style="font-size: 11px;">   
-			                	<div class="right">
-			                    <a href="<?php echo $helper->url("Roles","borrarId"); ?>&id_rol=<?php echo $res->id_rol; ?>" class="btn btn-danger" style="font-size:65%;"><i class="glyphicon glyphicon-trash"></i></a>
-			                </div>
-			                
-		               </td>
-		    		</tr>
-		        <?php } ?>
-            
-            
-            
-       	</table>     
-      </section>
-        </div>
-  </div></div>
-       
-        <footer class="col-lg-12">
-           <?php include("view/modulos/footer.php"); ?>
-        </footer>
+    <body  style="background-color: #F6FADE;"  onload="revisa_compras();">
+     <div class="wrapper">
+
+      <!-- Main Header -->
+      <header class="main-header">
+
+        <!-- Logo -->
+        <?php
+        include('view/modulos/head.php');
+        ?>
+
+      </header>
+      <!-- Left side column. contains the logo and sidebar -->
+      <aside class="main-sidebar">
+
+        <!-- sidebar: style can be found in sidebar.less -->
+        <?php
+        include('view/modulos/slide.php');
         
-     </body>  
-    </html>          
+        $dias = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","S�bado");
+        $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+        $fecha=$dias[date('w')]." ".date('d')." de ".$meses[date('n')-1]. " del ".date('Y') ;
+        
+        ?>
+        <!-- /.sidebar -->
+      </aside>
+
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+          <?php
+          echo "IndexDocument";
+          ?>
+            <small><?php echo $fecha; ?></small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="<?php echo $helper->url("usuarios","Loguear"); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Inicio</li>
+          </ol>
+        </section>
+
+        <!-- Para el contenido -->
+        <section class="content">
+        
+          <div class='row'>
+        	<div id="listadoRoles"> </div>
+          </div>
+
+        </section><!-- /.content -->
+      </div><!-- /.content-wrapper -->
+
+      <!-- Main Footer -->
+      <?php include('view/modulos/footer.php'); ?>
+      <!-- Add the sidebar's background. This div must be placed
+           immediately after the control sidebar -->
+      <div class="control-sidebar-bg"></div>
+    </div><!-- ./wrapper --> 
+    
+    <div class="MsjAjaxForm"></div>
+    <?php include "view/modulos/script.php"; ?>
+    <script src="view/adminLTE/plugins/morris/morris.min.js"></script>
+    <script src="view/adminLTE/plugins/morris/raphael-min.js"></script>
+    <script src="view/adminLTE/dist/js/source_init.js"></script> 
+ 		
+    </body>
+</html>
